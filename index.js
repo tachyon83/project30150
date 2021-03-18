52c41f9961d81d3e44946b605718c7804855ef0a
tachyon <83tachyon@gmail.com>
2021-03-18 16:25:57 +0900

    some other routes are added.

--- index.js
+++ index.js
@@ -78,7 +78,15 @@ app.use('/activities/:channelId', (req, res) => {
         //     // }
         //     return item.contentDetails
         // })
-        let ret = body.items.map(item => item.contentDetails)
+        // let ret = body.items.map(item => item.contentDetails)
+        let ret = body.items.map(item => {
+            return {
+                title: item.snippet.title,
+                description: item.snippet.description,
+                type: item.snippet.type,
+                videoId: item.contentDetails,
+            }
+        })
         console.log(ret)
 
         if (err) return res.status(500).json(err)
@@ -91,14 +99,343 @@ app.use('/activities/:channelId', (req, res) => {
     })
 })
 
-app.use('/:q/:type', (req, res) => {
-    req.params.q = qs.unescape(req.params.q)
+// about videos with topicDetails
+app.use('/topicdetails/:id', (req, res) => {
+    // req.query.maxResults = req.query.maxResults || null
+    // req.query.pageToken = req.query.pageToken || null
+    // // req.params.relatedToVideoId = qs.unescape(req.params.relatedToVideoId)
+    // req.params.id = qs.unescape(req.params.id)
+
+    // const url = process.env.base_url2 +
+    const url = process.env.base_url +
+        '/videos?' +
+        `id=${req.params.id}` +
+        '&part=snippet,contentDetails,statistics,status,topicDetails' +
+        //     (req.query.maxResults ? `&maxResults=${req.query.maxResults}` : '') +
+        //     (req.query.pageToken ? `&pageToken=${req.query.pageToken}` : '') +
+        `&key=${process.env.key}`
+
+    // url = `https://www.googleapis.com/youtube/v3/videos?id=7lCDEYXw3mM&part=snippet,topicDetails` + `&key=${process.env.key}`
+    console.log(url)
+
+    request({
+        url,
+        method: 'GET'
+    }, (err, response, body) => {
+
+        // console.log('response', response)
+        console.log('Status', response.statusCode);
+        body = JSON.parse(body)
+        console.log('prevPageToken', body.prevPageToken)
+        console.log('nextPageToken', body.nextPageToken)
+        // console.log('Headers', JSON.stringify(response.headers));
+        console.log('body', body)
+
+        // let ret = body.items.map(item => {
+        //     return {
+        //         videoId: item.id.videoId,
+        //         title: item.snippet.title,
+        //         snippet: item.snippet,
+        //         topicDetails: item.topicDetails,
+        //     }
+        //     // item.id.videoId
+        // })
+        let ret = body.items
+        console.log(ret)
+
+        if (err) return res.status(500).json(err)
+        if (response.statusCode !== 200) return res.status(response.statusCode).json(null)
+
+        // console.log(`[lRouter]: Now handling items found by q: ${req.params.q}...`)
+        console.log()
+
+        res.status(200).json(ret)
+        // res.status(200).json(body)
+    })
+})
+
+// about channels with topicDetails
+app.use('/channels/topicdetails/:id', (req, res) => {
+    // req.query.maxResults = req.query.maxResults || null
+    // req.query.pageToken = req.query.pageToken || null
+    // // req.params.relatedToVideoId = qs.unescape(req.params.relatedToVideoId)
+    // req.params.id = qs.unescape(req.params.id)
+
+    // const url = process.env.base_url2 +
+    const url = process.env.base_url +
+        '/channels?' +
+        `id=${req.params.id}` +
+        '&part=snippet,contentDetails,statistics,status,topicDetails' +
+        //     (req.query.maxResults ? `&maxResults=${req.query.maxResults}` : '') +
+        //     (req.query.pageToken ? `&pageToken=${req.query.pageToken}` : '') +
+        `&key=${process.env.key}`
+
+    // url = `https://www.googleapis.com/youtube/v3/videos?id=7lCDEYXw3mM&part=snippet,topicDetails` + `&key=${process.env.key}`
+    console.log(url)
+
+    request({
+        url,
+        method: 'GET'
+    }, (err, response, body) => {
+
+        // console.log('response', response)
+        console.log('Status', response.statusCode);
+        body = JSON.parse(body)
+        console.log('prevPageToken', body.prevPageToken)
+        console.log('nextPageToken', body.nextPageToken)
+        // console.log('Headers', JSON.stringify(response.headers));
+        console.log('body', body)
+
+        let ret = body.items.map(item => {
+            return {
+                videoId: item.id.videoId,
+                title: item.snippet.title,
+                snippet: item.snippet,
+                topicDetails: item.topicDetails,
+            }
+            // item.id.videoId
+        })
+        console.log(ret)
+
+        if (err) return res.status(500).json(err)
+        if (response.statusCode !== 200) return res.status(response.statusCode).json(null)
+
+        // console.log(`[lRouter]: Now handling items found by q: ${req.params.q}...`)
+        console.log()
+
+        res.status(200).json(ret)
+        // res.status(200).json(body)
+    })
+})
+
+// topic-based
+app.use('/topicbased', (req, res) => {
+    // req.query.maxResults = req.query.maxResults || null
+    // req.query.pageToken = req.query.pageToken || null
+    // // req.params.relatedToVideoId = qs.unescape(req.params.relatedToVideoId)
+    // req.params.id = qs.unescape(req.params.id)
+
+    console.log(req.query.topicId)
+
+    const url = process.env.base_url +
+        '/search?part=snippet' +
+        `&topicId=${req.query.topicId}` +
+        `&type=channel` +
+        //     (req.query.maxResults ? `&maxResults=${req.query.maxResults}` : '') +
+        //     (req.query.pageToken ? `&pageToken=${req.query.pageToken}` : '') +
+        `&key=${process.env.key}`
+
+    // url = `https://www.googleapis.com/youtube/v3/videos?id=7lCDEYXw3mM&part=snippet,topicDetails` + `&key=${process.env.key}`
+    console.log(url)
+
+    request({
+        url,
+        method: 'GET'
+    }, (err, response, body) => {
+
+        // console.log('response', response)
+        console.log('Status', response.statusCode);
+        body = JSON.parse(body)
+        console.log('prevPageToken', body.prevPageToken)
+        console.log('nextPageToken', body.nextPageToken)
+        // console.log('Headers', JSON.stringify(response.headers));
+        console.log('body', body)
+
+        // let ret = body.items.map(item => {
+        //     return {
+        //         videoId: item.id.videoId,
+        //         title: item.snippet.title,
+        //         snippet: item.snippet,
+        //         topicDetails: item.topicDetails,
+        //     }
+        //     // item.id.videoId
+        // })
+        let ret = body.items
+        console.log(ret)
+
+        if (err) return res.status(500).json(err)
+        if (response.statusCode !== 200) return res.status(response.statusCode).json(null)
+
+        // console.log(`[lRouter]: Now handling items found by q: ${req.params.q}...`)
+        console.log()
+
+        res.status(200).json(ret)
+        // res.status(200).json(body)
+    })
+})
+
+// videoCategories
+app.use('/videocategories', (req, res) => {
+    // req.query.maxResults = req.query.maxResults || null
+    // req.query.pageToken = req.query.pageToken || null
+    // // req.params.relatedToVideoId = qs.unescape(req.params.relatedToVideoId)
+    // req.params.id = qs.unescape(req.params.id)
+
+    console.log(req.query.topicId)
+
+    const url = process.env.base_url +
+        '/videoCategories?part=snippet' +
+        `&regionCode=KR` +
+        //     (req.query.maxResults ? `&maxResults=${req.query.maxResults}` : '') +
+        //     (req.query.pageToken ? `&pageToken=${req.query.pageToken}` : '') +
+        `&key=${process.env.key}`
+
+    // url = `https://www.googleapis.com/youtube/v3/videos?id=7lCDEYXw3mM&part=snippet,topicDetails` + `&key=${process.env.key}`
+    console.log(url)
+
+    request({
+        url,
+        method: 'GET'
+    }, (err, response, body) => {
+
+        // console.log('response', response)
+        console.log('Status', response.statusCode);
+        body = JSON.parse(body)
+        console.log('prevPageToken', body.prevPageToken)
+        console.log('nextPageToken', body.nextPageToken)
+        // console.log('Headers', JSON.stringify(response.headers));
+        console.log('body', body)
+
+        // let ret = body.items.map(item => {
+        //     return {
+        //         videoId: item.id.videoId,
+        //         title: item.snippet.title,
+        //         snippet: item.snippet,
+        //         topicDetails: item.topicDetails,
+        //     }
+        //     // item.id.videoId
+        // })
+        let ret = body.items
+        console.log(ret)
+
+        if (err) return res.status(500).json(err)
+        if (response.statusCode !== 200) return res.status(response.statusCode).json(null)
+
+        // console.log(`[lRouter]: Now handling items found by q: ${req.params.q}...`)
+        console.log()
+
+        res.status(200).json(ret)
+        // res.status(200).json(body)
+    })
+})
+
+// categoryId=28
+app.use('/28', (req, res) => {
+    // req.query.maxResults = req.query.maxResults || null
+    // req.query.pageToken = req.query.pageToken || null
+    // // req.params.relatedToVideoId = qs.unescape(req.params.relatedToVideoId)
+    // req.params.id = qs.unescape(req.params.id)
+
+    console.log(req.query.topicId)
+
+    const url = process.env.base_url +
+        // '/videos?part=snippet' +
+        '/videos?' +
+        `&chart=mostPopular` +
+        `&regionCode=KR` +
+        `&videoCategoryId=28` +
+        //     (req.query.maxResults ? `&maxResults=${req.query.maxResults}` : '') +
+        //     (req.query.pageToken ? `&pageToken=${req.query.pageToken}` : '') +
+        `&key=${process.env.key}`
+
+    // url = `https://www.googleapis.com/youtube/v3/videos?id=7lCDEYXw3mM&part=snippet,topicDetails` + `&key=${process.env.key}`
+    console.log(url)
+
+    request({
+        url,
+        method: 'GET'
+    }, (err, response, body) => {
+
+        // console.log('response', response)
+        console.log('Status', response.statusCode);
+        body = JSON.parse(body)
+        console.log('prevPageToken', body.prevPageToken)
+        console.log('nextPageToken', body.nextPageToken)
+        // console.log('Headers', JSON.stringify(response.headers));
+        console.log('body', body)
+
+        // let ret = body.items.map(item => {
+        //     return {
+        //         videoId: item.id.videoId,
+        //         title: item.snippet.title,
+        //         snippet: item.snippet,
+        //         topicDetails: item.topicDetails,
+        //     }
+        //     // item.id.videoId
+        // })
+        let ret = body.items
+        console.log(ret)
+
+        if (err) return res.status(500).json(err)
+        if (response.statusCode !== 200) return res.status(response.statusCode).json(null)
+
+        // console.log(`[lRouter]: Now handling items found by q: ${req.params.q}...`)
+        console.log()
+
+        res.status(200).json(ret)
+        // res.status(200).json(body)
+    })
+})
+
+app.use('/related/:relatedToVideoId', (req, res) => {
+    req.query.maxResults = req.query.maxResults || null
+    req.query.pageToken = req.query.pageToken || null
+    req.params.relatedToVideoId = qs.unescape(req.params.relatedToVideoId)
+
+    const url = process.env.base_url +
+        '/search?part=snippet' +
+        `&relatedToVideoId=${req.params.relatedToVideoId}` +
+        (req.query.maxResults ? `&maxResults=${req.query.maxResults}` : '') +
+        (req.query.pageToken ? `&pageToken=${req.query.pageToken}` : '') +
+        `&type=video` +
+        `&key=${process.env.key}`
+
+    console.log(url)
+
+    request({
+        url,
+        method: 'GET'
+    }, (err, response, body) => {
+
+        // console.log('response', response)
+        console.log('Status', response.statusCode);
+        body = JSON.parse(body)
+        console.log('prevPageToken', body.prevPageToken)
+        console.log('nextPageToken', body.nextPageToken)
+        // console.log('Headers', JSON.stringify(response.headers));
+        console.log('body', body)
+
+        let ret = body.items.map(item => {
+            return {
+                videoId: item.id.videoId,
+                title: item.snippet.title,
+            }
+            // item.id.videoId
+        })
+        console.log(ret)
+
+        if (err) return res.status(500).json(err)
+        if (response.statusCode !== 200) return res.status(response.statusCode).json(null)
+
+        console.log(`[lRouter]: Now handling items found by q: ${req.params.q}...`)
+        console.log()
+
+        res.status(200).json(ret)
+        // res.status(200).json(body)
+    })
+})
+
+app.use('/type/:q/:type', (req, res) => {
+    console.log(req.params.q)
+    req.params.q = qs.escape(req.params.q)
     req.params.type = qs.unescape(req.params.type)
     req.query.maxResults = req.query.maxResults || null
     req.query.pageToken = req.query.pageToken || null
 
     const url = process.env.base_url +
-        '/search?part=snippet' +
+        // '/search?part=snippet' +
+        '/search?' +
+        `part=snippet` +
         `&q=${req.params.q}` +
         `&type=${req.params.type}` +
         (req.query.maxResults ? `&maxResults=${req.query.maxResults}` : '') +
@@ -112,27 +449,87 @@ app.use('/:q/:type', (req, res) => {
         method: 'GET'
     }, (err, response, body) => {
 
+        if (err) return res.status(500).json(err)
+
+        // console.log('response', response)
+        console.log('Status', response.statusCode);
+        body = JSON.parse(body)
+        console.log('prevPageToken', body.prevPageToken)
+        console.log('nextPageToken', body.nextPageToken)
+        console.log('Headers', JSON.stringify(response.headers));
+        console.log('body', body)
+        if (body.error) console.log('body-err', body.error.errors)
+
+        // let ret = body.items.map(item => {
+        //     return {
+        //         channelTitle: item.snippet.channelTitle,
+        //         description: item.snippet.description,
+        //         channelId: item.snippet.channelId,
+        //     }
+        // })
+        let ret = body.items
+        console.log(ret)
+
+
+        if (response.statusCode !== 200) return res.status(response.statusCode).json(null)
+
+        console.log(`[Router]: Now handling items found by q: ${req.params.q}...`)
+        console.log()
+
+        res.status(200).json(ret)
+    })
+})
+
+// videos about a particular subject
+app.use('/particular/:subject', (req, res) => {
+
+    if (req.params) for (let e of Object.keys(req.params)) req.params[e] = qs.unescape(req.params[e])
+    req.query.maxResults = req.query.maxResults || null
+    req.query.pageToken = req.query.pageToken || null
+
+    // snippet,contentDetails,statistics,status,topicDetails
+
+    const url = process.env.base_url +
+        '/search?part=snippet' +
+        // `&q=YouTube+Data+API` +
+        `&q=${req.params.subject}` +
+        `&type=video` +
+        `&videoCaption=closedCaption` +
+        // (req.query.maxResults ? `&maxResults=${req.query.maxResults}` : '') +
+        // (req.query.pageToken ? `&pageToken=${req.query.pageToken}` : '') +
+        `&key=${process.env.key}`
+
+    console.log(url)
+
+    request({
+        url,
+        method: 'GET'
+    }, (err, response, body) => {
+
+        if (err) return res.status(500).json(err)
+
         // console.log('response', response)
         console.log('Status', response.statusCode);
         body = JSON.parse(body)
         console.log('prevPageToken', body.prevPageToken)
         console.log('nextPageToken', body.nextPageToken)
         // console.log('Headers', JSON.stringify(response.headers));
-        // console.log('body', body)
+        console.log('body', body)
 
-        let ret = body.items.map(item => {
-            return {
-                channelTitle: item.snippet.channelTitle,
-                description: item.snippet.description,
-                channelId: item.snippet.channelId,
-            }
-        })
+        // let ret = body.items.map(item => {
+        //     return {
+        //         channelTitle: item.snippet.channelTitle,
+        //         description: item.snippet.description,
+        //         channelId: item.snippet.channelId,
+        //     }
+        // })
+        let ret = body.items
         console.log(ret)
 
-        if (err) return res.status(500).json(err)
+
         if (response.statusCode !== 200) return res.status(response.statusCode).json(null)
 
-        console.log(`[lRouter]: Now handling items found by q: ${req.params.q}...`)
+        console.log(`[Router]: Now handling items found by a particular subject...`)
         console.log()
 
         res.status(200).json(ret)
